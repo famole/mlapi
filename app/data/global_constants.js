05c1eb18ce49c2a4c121dd9a6e302539376c464b
fabian molina <famole@gmail.com>
2017-11-02 15:17:23 -0300

    Isolated API

--- app/data/global_constants.js
+++
@@ -1,2 +0,0 @@
-export const apiUrl = 'http://localhost:4000/api/';
-export const PLACEHOLDER = 'Nunca dejes de buscar';
--- src/reducers/todo.js
+++
@@ -1,3 +0,0 @@
-export default function todos(state = [], action) {
-    return state;
-}
